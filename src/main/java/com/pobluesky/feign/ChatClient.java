9353af0a8c56f8ebe784b03a2a948ccbb1c70760
parlyresk <parlyresk325@gmail.com>
2024-10-10 21:40:12 +0900

    inquiry

--- src/main/java/com/pobluesky/feign/ChatClient.java
+++ src/main/java/com/pobluesky/feign/ChatClient.java
@@ -5,20 +5,22 @@ import com.pobluesky.inquiry.entity.ProductType;
 import java.util.List;
 import java.util.Map;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(name = "chat", configuration = FeignConfig.class)
 public interface ChatClient {
 
-    @PostMapping("/api/ocr/{userId}")
+    @PostMapping(value = "/api/ocr/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     JsonResult<Map<String, Object>> processOcrFile(
         @RequestHeader("Authorization") String token,
         @PathVariable("userId") Long userId,
-        @RequestParam("file") MultipartFile file,
+        @RequestPart("file") MultipartFile file,
         @RequestParam("productType") ProductType productType
     );
 
